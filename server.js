3e64cb029c86cfb73fd4f3c2cc5cef9159c1ce9b
Glitch (hello-express) <none>
2020-07-21 17:46:05 +0000

    👔🐗 Checkpoint ./public/style.css:2201711/17 ./views/index.ejs:2201711/122:5152800/624 ./server.js:2201711/840:5152800/234 ./views/partials/header.ejs:2201711/84:5152800/622

--- server.js
+++ server.js
@@ -1,15 +1,42 @@
 const express = require("express");
 const app = express();
-
+const jsoning = require("jsoning");
 let ejs = require("ejs");
+
+let db = new jsoning("db.json");
+
+
+var pages = [];
+
+
+//********************************************************************************************************************************************************************************************************************************************************************
 
 app.use(express.static("public"));
 app.set("view engine", ejs);
-// https://expressjs.com/en/starter/basic-routing.html
+
+function checkHttps(req, res, next){
+  if(req.get('X-Forwarded-Proto').indexOf("https")!=-1){
+    return next()
+  } else {
+    res.redirect('https://' + req.hostname + req.url);
+  }
+}
+
+app.all('*', checkHttps);
+
 app.get("/", (request, response) => {
   response.render(__dirname + "/views/index.ejs");
 });
 
+app.get("/s", (request,response) => {
+  response.render()
+})
+
+
+
+
 const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
+  if (!db.has("List-Of-Sites")){
+    db.set("List-Of-Sites", []);
+  }
 });
